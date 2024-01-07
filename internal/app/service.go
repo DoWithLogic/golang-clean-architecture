5ae18311645b811709e6097719eab442de43e580
martinyonatann <martin.yonathan305@gmail.com>
2024-01-07 11:33:39 +0700

    chore: add graceful Shutdown and healthcheck

--- internal/app/service.go
+++ internal/app/service.go
@@ -1,17 +1,23 @@
 package app
 
 import (
+	"net/http"
+
 	userV1 "github.com/DoWithLogic/golang-clean-architecture/internal/users/delivery/http/v1"
 	userRepository "github.com/DoWithLogic/golang-clean-architecture/internal/users/repository"
 	userUseCase "github.com/DoWithLogic/golang-clean-architecture/internal/users/usecase"
+	"github.com/labstack/echo/v4"
 )
 
-func (app *App) StartService() error {
+func (app *App) startService() error {
 	userRepo := userRepository.NewRepository(app.db)
 	userUC := userUseCase.NewUseCase(userRepo, app.cfg)
 	userCTRL := userV1.NewHandlers(userUC)
 
 	domain := app.echo.Group("/api/v1/users")
+	domain.GET("/ping", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello Word 👋")
+	})
 
 	userCTRL.UserRoutes(domain, app.cfg)
 
